2d13976980f7a9bba9265d79e2121ac78cde103d
DermotBoyle <48441291+DermotBoyle@users.noreply.github.com>
2019-04-24 17:25:02 +0200

    finished travels

---
+++ src/Travel.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Travel = (props) => {
+  const {image, destination, country, distance} = props;
+  return (
+    <figure>
+      <img src={image} alt={destination} />
+      <figcaption>
+        <h1>{country}</h1>
+        <h2>{destination}</h2>
+        <h2>{distance}</h2>  
+      </figcaption>
+    </figure>
+  )
+};
+
+export default Travel;
--- src/Travels.js
+++ src/Travels.js
@@ -1,14 +1,40 @@
 import React from "react";
+import Travel from "./Travel";
 
-const Travels = props => (
-  <figure>
-    <img src={props.image} alt={props.destination} />
-    <figcaption>
-      <h1>{props.country}</h1>
-      <h2>{props.destination}</h2>
-      <h2>{props.distance}</h2>  
-    </figcaption>
-  </figure>
-);
+const arr = [
+{
+ country: "USA",
+ destination : "Destination: New York",
+ distance: "Distance: 1650km",
+ image : "https://images.pexels.com/photos/1486222/pexels-photo-1486222.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+country : "Brazil",
+ destination : "Destination: Rio de Janiero",
+ distance : "Distance: 1200km",
+ image : "https://images.pexels.com/photos/1959068/pexels-photo-1959068.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+},
+{
+  country : "Australia",
+   destination : "Melbourne",
+   distance : "Distance: 2400km",
+   image : "https://images.pexels.com/photos/1959068/pexels-photo-1959068.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    country : "Ireland",
+     destination : "Derry",
+     distance : "Distance: 800km",
+     image : "https://images.pexels.com/photos/1959068/pexels-photo-1959068.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    },
+]; 
+
+
+const Travels = () => (
+      <div>
+          {arr.map(countryData => (
+            <Travel {...countryData} />
+          ))}
+        </div>
+)
 
 export default Travels;
